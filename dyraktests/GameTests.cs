8853932ae736e65cf82293dc20baff09d7fa4a07
vovchenko-varya <varvaravovchenko111@gmail.com>
2026-04-29 10:19:09 +0300

    Initial commit

---
+++ dyraktests/GameTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DurakLibrary;
+
+namespace DurakTests
+{
+    [TestClass]
+    public class GameTests
+    {
+        [TestMethod]
+        public void NewGame_ShouldGiveSixCards()
+        {
+            Game game = new Game();
+            game.NewGame();
+            Assert.AreEqual(6, game.HumanHand.Count);
+            Assert.AreEqual(6, game.ComputerHand.Count);
+        }
+
+        [TestMethod]
+        public void NewGame_ShouldHaveTrumpCard()
+        {
+            Game game = new Game();
+            game.NewGame();
+            Assert.IsNotNull(game.TrumpCard);
+        }
+    }
+}
